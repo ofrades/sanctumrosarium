527b97d34696208fdd86a9e9120b32bd38ebed83
miguel/ofrades <mig.silva@gmail.com>
2020-12-02 08:07:13 +0000

    feat: add static data

--- app/pages/missa.tsx
+++ app/pages/missa.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import fs from 'fs'
+import path from 'path'
 
 const Container = styled.div`
   display: flex;
@@ -12,12 +14,40 @@ const Container = styled.div`
   }
 `
 
-const Missa = () => {
+const Missa = ({ massFile }) => {
+  const mass = JSON.parse(massFile)
+
+  const [a:[info, sections]] = mass
+  const id: [string] = a.sections.map(b => b.id)
+  const body: [string] = a.sections.map(b => b.body)
+
   return (
     <Container>
-      <h1>Missa</h1>
+      <h1> Title: {a.info.title}</h1>
+      <h1> Date: {a.info.date}</h1>
+      <h5>{id}</h5>
+      <ul>
+        {a.sections.map((c) => 
+          <li key={c.id}>id: {c.id}</li>
+        )}
+      </ul>
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const date = new Date()
+  const today = `${date.getFullYear()}-${date.getMonth() + 1}-${(
+    '0' + date.getDate()
+  ).slice(-2)}`
+  const dir = path.join(process.cwd(), 'public/date/')
+  const filePath = path.join(dir, today)
+  const massFile = fs.readFileSync(filePath, 'utf8')
+  return {
+    props: {
+      massFile,
+    },
+  }
 }
 
 export default Missa
